dfceef6eed80f8b5d690c95a205d9b42a6683753
Anup Kumar Singh <88195114+akm005@users.noreply.github.com>
2024-05-14 23:21:49 +0530

    -- added about section :TODO text alignment-fix animation fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './header/header.tsx';
 import HeroSection from './heroSection/heroSection.tsx'
+import AboutUs from './aboutUs/aboutUs.tsx';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <HeroSection/>
+      <AboutUs/>
     </div>
   );
 }
